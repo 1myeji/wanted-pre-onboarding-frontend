577e2e9adff25b803cfa1c7d60aedc46a2535908
imyeji <yimyg0596@naver.com>
2023-04-16 19:52:01 +0900

    Feat: 로그인 여부에 따른 리다이렉트 처리

--- src/Router.js
+++ src/Router.js
@@ -4,10 +4,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Layout from "./components/common/Layout";
 import Todo from "./pages/Todo";
+import Redirect from "./pages/Redirect";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Redirect />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/signin" element={<SignIn />} />
---
+++ src/pages/Redirect.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const Redirect = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const accessToken = localStorage.getItem("accessToken");
+  useEffect(() => {
+    if (
+      accessToken &&
+      (location.pathname === "/signin" || location.pathname === "/signup")
+    )
+      navigate("/todo");
+    if (!accessToken && location.pathname === "/todo") navigate("/signin");
+  }, [location, navigate, accessToken]);
+
+  return null;
+};
+
+export default Redirect;
